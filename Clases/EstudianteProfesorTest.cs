775ed6d859b6cafb649f95f4ed424ff28cb962cf
quime <quime@QUIMEY>
2024-08-25 21:53:49 -0300

    Agregar archivos de proyecto.

---
+++ Clases/EstudianteProfesorTest.cs
@@ -0,0 +1,26 @@
+﻿using Clases;
+using System;
+
+namespace ProyectoPersonas
+{
+    public class EstudianteProfesorTest
+    {
+        public static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.Saludar();
+
+            Estudiante estudiante = new Estudiante();
+            estudiante.SetEdad(20);
+            estudiante.Saludar();
+            estudiante.VerEdad();
+            estudiante.Estudiar();
+
+            Profesor profesor = new Profesor();
+            profesor.SetEdad(45);
+            profesor.Saludar();
+            profesor.Explicar();
+        }
+    }
+}
+
---
+++ Clases/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Persona
+    {
+        protected int edad;
+
+        public void Saludar()
+        {
+            Console.WriteLine("Hola");
+        }
+
+        public void SetEdad(int edad)
+        {
+            this.edad = edad;
+        }
+    }
+}
